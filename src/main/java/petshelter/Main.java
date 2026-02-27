789c163b837a2c95d0cac6a2a89e27cbe154591e
Khanh Haines <dthaine1@asu.edu>
2026-02-27 16:24:10 -0600

    New main test along wit selter class that initializes 5 animals to the shelter with different statuses

--- src/main/java/petshelter/Main.java
+++ src/main/java/petshelter/Main.java
@@ -1,7 +1,20 @@
 package petshelter;
 
+import petshelter.animals.Animal;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("PetShelter system running");
+        Shelter shelter = new Shelter();
+
+        System.out.println("SIMS I");
+
+        for (Animal animal : shelter.getAnimals()) {
+            System.out.println(
+                    animal.getId() + " - " +
+                            animal.getSpecies() + " - " +
+                            animal.getStatusName()
+            );
+        }
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.DisplayName;
+import petshelter.animals.Animal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
@@ -10,4 +12,17 @@ public class MainTest {
     void tQ1BasicBuildTest() {
         assertTrue(true);
     }
+
+
+    @Test
+    void T_Q1_basicBuildTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void T_Q2_animalDefaultStateIsIntake() {
+        Animal animal = new Animal("A100", "Dog", 2, "Healthy", "SZ-001");
+        assertEquals("INTAKE", animal.getStatusName());
+    }
+
 }
