b2df83636601db36ffa941a91e31edf64e4c3715
Khanh Haines <dthaine1@asu.edu>
2026-02-27 18:19:08 -0600

    Fix class name mismatch: renamed PetShelterEngineTest to SimulationEngineTest for better organization.

--- src/test/java/petsheltertests/SimulationEngineTest.java
+++ src/test/java/petsheltertests/SimulationEngineTest.java
@@ -1,4 +1,17 @@
 package petsheltertests;
 
-public class PetShelterEngineTest {
+import org.junit.jupiter.api.Test;
+import petshelter.Shelter;
+import petshelter.SimulationEngine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimulationEngineTest {
+
+    @Test
+    void T_Q3_romanNumeralConversionWorks() {
+        SimulationEngine engine = new SimulationEngine(new Shelter());
+        assertEquals("I", engine.toRoman(1));
+        assertEquals("VII", engine.toRoman(7));
+    }
 }
